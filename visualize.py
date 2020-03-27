e337b9733beee1ad4434294c9fa83e80e33121a6
codeocean <1264044626@qq.com>
2020-03-27 16:00:28 +0800

    val len and print net size

--- visualize.py
+++ visualize.py
@@ -1,22 +1,13 @@
-import numpy as np
-import torchvision
-import time
-import os
-import copy
-import pdb
-import time
 import argparse
+import time
 
-import sys
 import cv2
-
+import numpy as np
 import torch
-from torch.utils.data import Dataset, DataLoader
-from torchvision import datasets, models, transforms
+from torchvision import transforms
 
-from retinanet.dataloader import CocoDataset, CSVDataset, collater, Resizer, AspectRatioBasedSampler, Augmenter, \
-	UnNormalizer, Normalizer
-
+from retinanet import model
+from retinanet.dataloader import CocoDataset, Resizer, Normalizer
 
 assert torch.__version__.split('.')[0] == '1'
 
@@ -24,85 +15,75 @@ print('CUDA available: {}'.format(torch.cuda.is_available()))
 
 
 def main(args=None):
-	parser = argparse.ArgumentParser(description='Simple training script for training a RetinaNet network.')
+    parser = argparse.ArgumentParser(description='Simple training script for training a RetinaNet network.')
 
-	parser.add_argument('--dataset', help='Dataset type, must be one of csv or coco.')
-	parser.add_argument('--coco_path', help='Path to COCO directory')
-	parser.add_argument('--csv_classes', help='Path to file containing class list (see readme)')
-	parser.add_argument('--csv_val', help='Path to file containing validation annotations (optional, see readme)')
+    parser.add_argument('--coco_path', help='Path to COCO directory')
+    parser.add_argument('--model_path', help='Path to model', type=str)
 
-	parser.add_argument('--model', help='Path to model (.pt) file.')
+    parser = parser.parse_args(args)
 
-	parser = parser.parse_args(args)
+    dataset_val = CocoDataset(parser.coco_path, set_name='val2017',
+                              transform=transforms.Compose([Normalizer(), Resizer()]))
 
-	if parser.dataset == 'coco':
-		dataset_val = CocoDataset(parser.coco_path, set_name='train2017', transform=transforms.Compose([Normalizer(), Resizer()]))
-	elif parser.dataset == 'csv':
-		dataset_val = CSVDataset(train_file=parser.csv_train, class_list=parser.csv_classes, transform=transforms.Compose([Normalizer(), Resizer()]))
-	else:
-		raise ValueError('Dataset type not understood (must be csv or coco), exiting.')
+    # Create the model
+    retinanet = model.resnet50(num_classes=dataset_val.num_classes(), pretrained=True)
 
-	sampler_val = AspectRatioBasedSampler(dataset_val, batch_size=1, drop_last=False)
-	dataloader_val = DataLoader(dataset_val, num_workers=1, collate_fn=collater, batch_sampler=sampler_val)
+    use_gpu = True
 
-	retinanet = torch.load(parser.model)
+    if use_gpu:
+        if torch.cuda.is_available():
+            retinanet = retinanet.cuda()
 
-	use_gpu = True
+    if torch.cuda.is_available():
+        retinanet.load_state_dict(torch.load(parser.model_path))
+        retinanet = torch.nn.DataParallel(retinanet).cuda()
+    else:
+        retinanet.load_state_dict(torch.load(parser.model_path))
+        retinanet = torch.nn.DataParallel(retinanet)
 
-	if use_gpu:
-		if torch.cuda.is_available():
-			retinanet = retinanet.cuda()
+    retinanet.training = False
+    retinanet.eval()
+    retinanet.module.freeze_bn()
 
-	if torch.cuda.is_available():
-		retinanet = torch.nn.DataParallel(retinanet).cuda()
-	else:
-		retinanet = torch.nn.DataParallel(retinanet)
+    def draw_caption(image, box, caption):
 
-	retinanet.eval()
+        b = np.array(box).astype(int)
+        cv2.putText(image, caption, (b[0], b[1] - 10), cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 0), 2)
+        cv2.putText(image, caption, (b[0], b[1] - 10), cv2.FONT_HERSHEY_PLAIN, 1, (255, 255, 255), 1)
 
-	unnormalize = UnNormalizer()
+    for idx, data in enumerate(dataset_val):
 
-	def draw_caption(image, box, caption):
+        with torch.no_grad():
+            st = time.time()
+            # run network
 
-		b = np.array(box).astype(int)
-		cv2.putText(image, caption, (b[0], b[1] - 10), cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 0), 2)
-		cv2.putText(image, caption, (b[0], b[1] - 10), cv2.FONT_HERSHEY_PLAIN, 1, (255, 255, 255), 1)
+            if torch.cuda.is_available():
+                scores, labels, boxes = retinanet(data['img'].permute(2, 0, 1).cuda().float().unsqueeze(dim=0))
+            else:
+                scores, labels, boxes = retinanet(data['img'].permute(2, 0, 1).float().unsqueeze(dim=0))
+            print('Elapsed time: {}'.format(time.time() - st))
+            idxs = np.where(scores.cpu() > 0.5)
 
-	for idx, data in enumerate(dataloader_val):
+            tensor = data['img'] * np.array([[[0.229, 0.224, 0.225]]]) + np.array([[[0.485, 0.456, 0.406]]])
+            img = tensor.mul(255).clamp(0, 255).byte().cpu().numpy()
 
-		with torch.no_grad():
-			st = time.time()
-			if torch.cuda.is_available():
-				scores, classification, transformed_anchors = retinanet(data['img'].cuda().float())
-			else:
-				scores, classification, transformed_anchors = retinanet(data['img'].float())
-			print('Elapsed time: {}'.format(time.time()-st))
-			idxs = np.where(scores.cpu()>0.5)
-			img = np.array(255 * unnormalize(data['img'][0, :, :, :])).copy()
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-			img[img<0] = 0
-			img[img>255] = 255
+            for j in range(idxs[0].shape[0]):
+                bbox = boxes[idxs[0][j], :]
+                x1 = int(bbox[0])
+                y1 = int(bbox[1])
+                x2 = int(bbox[2])
+                y2 = int(bbox[3])
+                label_name = dataset_val.labels[int(labels[idxs[0][j]])]
+                draw_caption(img, (x1, y1, x2, y2), label_name)
 
-			img = np.transpose(img, (1, 2, 0))
+                cv2.rectangle(img, (x1, y1), (x2, y2), color=(0, 0, 255), thickness=2)
+                print(label_name)
 
-			img = cv2.cvtColor(img.astype(np.uint8), cv2.COLOR_BGR2RGB)
-
-			for j in range(idxs[0].shape[0]):
-				bbox = transformed_anchors[idxs[0][j], :]
-				x1 = int(bbox[0])
-				y1 = int(bbox[1])
-				x2 = int(bbox[2])
-				y2 = int(bbox[3])
-				label_name = dataset_val.labels[int(classification[idxs[0][j]])]
-				draw_caption(img, (x1, y1, x2, y2), label_name)
-
-				cv2.rectangle(img, (x1, y1), (x2, y2), color=(0, 0, 255), thickness=2)
-				print(label_name)
-
-			cv2.imshow('img', img)
-			cv2.waitKey(0)
-
+            cv2.imshow('img', img)
+            cv2.waitKey(0)
 
 
 if __name__ == '__main__':
- main()
+    main()
